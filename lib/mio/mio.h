18299b2671a2a01db0bfe449d964be5a36315846
윤귀영 <dbsrnldud09@gmail.com>
2026-06-23 19:25:24 +0900

    chore : lib mio.h 추가

---
+++ lib/mio/mio.h
@@ -0,0 +1,68 @@
+#ifndef __IO_H__
+#define __IO_H__
+
+// INPUT PIN
+#define I00 22
+#define I01 23
+#define I02 24
+#define I03 25
+#define I04 26
+#define I05 27
+#define I06 28
+#define I07 29
+#define I08 30
+#define I09 31
+#define I10 32
+#define I11 33
+#define I12 34
+#define I13 35
+#define I14 36
+#define I15 37
+#define I16 38
+#define I17 39
+#define I18 40
+#define I19 41
+#define I20 42
+#define I21 43
+#define I22 44
+#define I23 45
+
+// OUTPUT PIN
+#define Q00 46
+#define Q01 47
+#define Q02 48
+#define Q03 49
+#define Q04 58
+#define Q05 59
+#define Q06 60
+#define Q07 61
+#define Q08 62
+#define Q09 63
+#define Q10 64
+#define Q11 65
+#define Q12 66
+#define Q13 67
+#define Q14 68
+#define Q15 69
+#define Q16 2
+#define Q17 3
+#define Q18 4
+#define Q19 5
+#define Q20 6
+#define Q21 7
+#define Q22 8
+#define Q23 9
+
+
+// PWM
+#define PWM0  11
+#define PWM1  12
+#define PWM2  13  
+
+// ADC
+#define ADC0  A0
+#define ADC1  A1
+#define ADC2  A2
+#define ADC3  A3
+
+#endif
